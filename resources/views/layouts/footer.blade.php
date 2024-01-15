9f918f862447ad0cb2da60bd1e9e1c699eca7f89
irhamauliaq <124588299+irhamauliaq@users.noreply.github.com>
2024-01-15 20:56:31 +0700

    pertama

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 1.1
+    </div>
+    <strong>Development &copy; {{ date('Y') }} <a href="/">{{ $setting->nama_perusahaan }}
+    </strong>
+
+</footer>
